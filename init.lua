a30a1e7e3faed68751c966af20ffe72caaefb71e
Panquesito7 <halfpacho@gmail.com>
2019-11-11 14:09:52 -0600

    Use translation method from MT 5.0.0+

--- init.lua
+++ init.lua
@@ -95,10 +95,9 @@ travelnet.targets = {};
 travelnet.path = minetest.get_modpath(minetest.get_current_modname())
 
 
--- Intllib
-local S = dofile(travelnet.path .. "/intllib.lua")
+-- Minetest Translator
+local S = minetest.get_translator("travelnet")
 travelnet.S = S
-
 
 minetest.register_privilege("travelnet_attach", { description = S("allows to attach travelnet boxes to travelnets of other players"), give_to_singleplayer = false});
 minetest.register_privilege("travelnet_remove", { description = S("allows to dig travelnet boxes which belog to nets of other players"), give_to_singleplayer = false});
