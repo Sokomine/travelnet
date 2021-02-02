5549df3accb2d2b8e7606d751c2c025118d30064
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-02-02 09:20:48 +0100

    fix player variable leak

--- functions.lua
+++ functions.lua
@@ -64,7 +64,13 @@ travelnet.open_close_door = function( pos, player, mode )
 		end
 
 		if mode == 2 then
-			minetest.after( 1, minetest.registered_nodes[ door_node.name ].on_rightclick, pos2, door_node, player );
+			local playername = player:get_player_name()
+			minetest.after(1, function()
+				local pplayer = minetest.get_player_by_name(playername)
+				if player then
+					minetest.registered_nodes[ door_node.name ].on_rightclick(pos2, door_node, pplayer);
+				end
+			end);
 		else
 			minetest.registered_nodes[ door_node.name ].on_rightclick(pos2, door_node, player);
 		end
